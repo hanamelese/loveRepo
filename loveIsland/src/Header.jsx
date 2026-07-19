f5bfb3ce90b5d98afb86e89320c59d5594f7ed33
hanamelese <hanamelese95@gmail.com>
2026-07-19 10:29:32 -0700

    finalize the remaining parts

--- loveIsland/src/Header.jsx
+++ loveIsland/src/Header.jsx
@@ -3,16 +3,8 @@ import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const navigate = useNavigate();
-
-  // Define your styles as a JavaScript object
   const styles = {
-    header: {
-      padding: '5px',
    
-      display: 'flex',
-      gap: '230px',
-    //   borderBottom: '2px solid #5a4a42'
-    },
     button: {
       backgroundColor: '#212121',
       color: '#fff',
@@ -22,13 +14,14 @@ const Header = () => {
       borderRadius: '4px',
       fontSize: '26px',
       fontWeight: 'bold',
+      left:0,
     }
   };
 
   return (
     <header style={styles.header}>
       <button style={styles.button} onClick={() => navigate(-1)}>← </button>
-      <button style={styles.button} onClick={() => navigate(1)}> →</button>
+      {/* <button style={{...styles.button, color: 'black'}} onClick={() => navigate(1)}> .</button>  */}
     </header>
   );
 };
--- loveIsland/src/loveDave.jsx
+++ loveIsland/src/loveDave.jsx
@@ -10,11 +10,12 @@ const LoveDave = () => {
       <div className="love-container">
          <Header />
        
-                    <div className="envelope" style={{ backgroundImage: `url(${envelope})` }}> I wanted to take a moment to tell you just how much you mean to me. 
-                      You are my joy, my calm, and my favorite adventure. Thank you for being the person who makes every challenge feel easier just by being by my side.</div>
-                    
-
-                
+                    <div className="envelope" > 
+                      <p>I wanted to take a moment to tell you just how much you mean to me. 
+                      You are my joy, my calm, and my favorite adventure. Thank you for being the person who makes every challenge feel easier just by being by my side.
+                        </p>
+                      <p> My sweet dave 😊😊</p></div>
+                        
       </div>
     </div>
   );
